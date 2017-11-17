025ac90a967617f74da0daaf2e88d46d58d2d85f
Jony13Molina <molinacjony@gmail.com>
2017-11-16 16:36:43 -0800

    Initial

--- Suggest/app/src/main/java/com/example/jonny/suggest/MainActivity.java
+++ Suggest/app/src/main/java/com/example/jonny/suggest/MainActivity.java
@@ -1,24 +1,56 @@
 package com.example.jonny.suggest;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity  implements View.OnClickListener{
+import android.widget.Toast;
+
+//libraries for the map
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener,
+        GoogleMap.OnMyLocationClickListener, OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener {
+
+    public GoogleApiClient mGooogleApiClient;
+    private GoogleMap mMap;
+    private static final int MY_LOCATION_REQUEST_CODE = 1;
+    private static final int REQUEST_PERMISSION = 2;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button searchB = (Button)findViewById(R.id.SearchButton);
-        searchB.setOnClickListener(this);
-        Button directions = (Button)findViewById(R.id.DirectionsB);
-        directions.setOnClickListener(this);
 
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        Button searchB = (Button) findViewById(R.id.SearchButton);
+        searchB.setOnClickListener(this);
+        Button directions = (Button) findViewById(R.id.DirectionsB);
+        directions.setOnClickListener(this);
 
     }
 
@@ -26,15 +58,114 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
     @Override
     public void onClick(View v) {
 
-        switch(v.getId())
-        {
+        switch (v.getId()) {
             case R.id.SearchButton:
-                Intent searchIntent = new Intent(MainActivity.this, SuggestionActivity.class);
+                Intent searchIntent = new Intent(MainActivity.this, MapsActivity.class);
                 startActivity(searchIntent);
             case R.id.DirectionsB:
                 Intent directionIntent = new Intent(MainActivity.this, Directions.class);
                 startActivity(directionIntent);
         }
 
+
     }
+
+
+   /* protected void googleApiClient(){
+       mGooogleApiClient = new GoogleApiClient.Builder(MainActivity.this)
+               .addConnectionCallbacks(this)
+               .addOnConnectionFailedListener(this);
+               .addApi(LocationServices.API)
+               .build();
+               */
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+
+        if (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                AlertDialog.Builder dlgAlert  = new AlertDialog.Builder(this);
+
+                dlgAlert.setMessage("We Need Location to Suggest Places");
+                dlgAlert.setTitle("Error Message...");
+                dlgAlert.setPositiveButton("OK", null);
+                dlgAlert.setCancelable(true);
+                dlgAlert.create().show();
+
+                dlgAlert.setPositiveButton("Ok",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                //check permission
+
+                            }
+                        });
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission
+                        .ACCESS_FINE_LOCATION},MY_LOCATION_REQUEST_CODE);
+            } else {
+                ActivityCompat.requestPermissions(
+                        this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_LOCATION_REQUEST_CODE);
+            }
+
+        } else {
+            mMap.setMyLocationEnabled(true);
+            mMap.setOnMyLocationButtonClickListener(this);
+            mMap.setOnMyLocationClickListener(this);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions,
+                                           int[] grantResults) {
+        if (requestCode == MY_LOCATION_REQUEST_CODE)
+        {
+            if (grantResults.length == 1
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // We can now safely use the API we requested access to
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission
+                        (this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED)
+                {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+
+                    //check permission
+                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION
+                    }, MY_LOCATION_REQUEST_CODE);
+
+
+                }
+                mMap.setMyLocationEnabled(true);
+            }
+
+        }
+    }
+    @Override
+    public void onMyLocationClick(@NonNull Location location) {
+        Toast.makeText(this, "Current location:\n" + location, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show();
+        // Return false so that we don't consume the event and the default behavior still occurs
+        // (the camera animates to the user's current position).
+        return false;
+    }
+
+
 }
